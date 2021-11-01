c430ac7cfe1fef43159e2ceea6fb8f087791bd3e
Aleksei Allen <aqml@mail.ru>
2021-11-01 21:51:07 +0300

    Add project files.

---
+++ HomeWork22/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeWork22
+{
+    //Сформировать массив случайных целых чисел.
+    class Program
+    {
+        static void GetSum(object[] arrayIn)
+        {
+            int[] array = arrayIn.Cast<int>().ToArray();
+            int sum = array.Sum();
+            Console.WriteLine("Cумма значений элементов масcива: {0}", sum);
+        }
+
+        static void GetMax(Task task, object[] arrayIn)
+        {
+            int[] array = arrayIn.Cast<int>().ToArray();
+            int max = array.Max();
+            Console.WriteLine("Максимальное значение элемента масcива: {0}", max);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер одномерного массива: ");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[arraySize];
+
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 100);
+            }
+
+            Console.WriteLine("Значения элементов массива: ");
+            foreach (int val in array)
+            {
+                Console.WriteLine(val + " ");
+            }
+            Console.WriteLine("");
+
+            object[] array1 = array.Cast<object>().ToArray();
+
+
+            Action<object[]> action1 = new Action<object[]>(GetSum);
+            Task task1 = new Task((Action<object>)action1, array1);
+
+            Action<Task, object[]> action2 = new Action<Task, object[]>(GetMax);
+            Task task2 = task1.ContinueWith((Action<Task, object>)action2, array1);
+
+            task1.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
